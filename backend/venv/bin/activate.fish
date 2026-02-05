ace1411ee2e47bf2dd56e9afc9342ac13e853f3f
nooele2 <noellehtoo@gmail.com>
2026-02-05 10:11:24 +0700

    Add gunicorn for production

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/ice-cream/bell-webapp/backend/venv"
+set -gx VIRTUAL_ENV "/Users/ice-cream/Downloads/bell-webapp/bell-webapp/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
